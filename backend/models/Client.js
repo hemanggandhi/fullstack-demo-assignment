d79376803f772154fac6b432b95af1287c559cc3
unknown <hemangsgandhi@gmail.com>
2025-07-04 16:08:43 +0530

    Initial commit

--- backend/models/Client.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  name: { type: String, unique: true, required: true },
-});
-module.exports = mongoose.model("Client", UserSchema);
